f153e4148146328e73ffa285077062c133f5fa0c
Harshitkushwah3 <harshitkushwah51@gmail.com>
2024-04-15 17:46:16 +0530

    upd3

--- src/Sorting/Bubblesort.java
+++ src/Sorting/Bubblesort.java
@@ -9,12 +9,33 @@ In Bubble Sort algorithm,
 */
 package Sorting;
 
+import java.util.Arrays;
+
 public class Bubblesort {
     public static void main(String[] args) {
-        int[] arr={13,46,24,52,20,9};
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
 
+        //other method to print the values
+
+//        for (int i = 0; i < arr.length; i++) {
+//            if (i < arr.length - 1) {
+//                System.out.print(arr[i] + ", ");
+//            } else {
+//                System.out.print(arr[i]);
+//            }
+//        }
     }
-    static int sort(int [] arr){
-        return -1;
+    static void sort(int [] arr){
+         for (int i=0;i<arr.length;i++){
+             for(int j=1;j<arr.length-i;j++){
+                 if (arr[j]<arr[j-1]){
+                     int temp=arr[j];
+                     arr[j]=arr[j-1];
+                     arr[j-1]=temp;
+                 }
+             }
+         }
     }
 }
